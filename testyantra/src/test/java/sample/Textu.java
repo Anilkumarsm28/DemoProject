4ecc071af853fc6eaae57f0acc8677de84e59d6a
ASUS <ASUS@DESKTOP-EA3O7R6>
2022-09-20 15:39:16 +0530

    aaaaa

--- testyantra/src/test/java/sample/Textu.java
+++ testyantra/src/test/java/sample/Textu.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Text   {
+public class Textu   {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
