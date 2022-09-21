e0169c52ff6d488442688761738e695c8ced55fb
ASUS <ASUS@DESKTOP-EA3O7R6>
2022-09-21 12:37:33 +0530

    Hiii

--- testyantra/src/test/java/sample/TextBoxuu.java
+++ testyantra/src/test/java/sample/TextBoxuu.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Textu   {
+public class TextBoxuu   {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
