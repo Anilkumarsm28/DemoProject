42266a79bf2fa56bcf30c4c9735c9c04516a1270
ASUS <ASUS@DESKTOP-EA3O7R6>
2022-09-21 13:24:53 +0530

    I have updated

--- testyantra/src/test/java/sample/TextBoxx.java
+++ testyantra/src/test/java/sample/TextBoxx.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TextBoxx {
+public class TextBoxx  {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
