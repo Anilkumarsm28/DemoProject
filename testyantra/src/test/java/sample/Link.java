1d3cff6d5a3a3b5744fc532015df924409f31828
preetishabadi <108131783+preetishabadi@users.noreply.github.com>
2022-09-22 10:44:06 +0530

    Update Link.java

--- testyantra/src/test/java/sample/Link.java
+++ testyantra/src/test/java/sample/Link.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Link {
+	
 	public static void main(String[] args) {
+		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://demoqa.com/links");
