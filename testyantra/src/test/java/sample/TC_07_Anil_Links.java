2089ae8944b6b729b52353fe9ba13a533148015a
Anilkumarsm28 <anilkumarsm28@gmail.com>
2022-09-22 10:43:19 +0530

    Updated Test script on Links

--- testyantra/src/test/java/sample/TC_07_Anil_Links.java
+++ testyantra/src/test/java/sample/TC_07_Anil_Links.java
@@ -1,15 +1,32 @@
 package sample;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_07_Anil_Links {
-	public static void main(String[] args) {
+	@Test
+	public void Linkspage() {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://demoqa.com/links");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		System.out.println("Browser launched successfully");
+
+		// opening links page
+		driver.get("https://demoqa.com/links");
+		System.out.println("Links page opened successfully");
+
+		// clicking on "Home" link
+		driver.findElement(By.id("simpleLink")).click();
+		System.out.println("clicked on Home link susccessfully");
+
+		driver.close();
+
 	}
 }
